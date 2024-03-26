fb98e1ada68a30b12e5755eb14be4ded538a1a7f
duongdang2003 <duonghaidang01012003@gmail.com>
2024-03-26 08:28:09 +0700

    add ignore file

---
+++ Fotos/Controllers/ShopItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fotos.Controllers
+{
+    public class ShopItemController : Controller
+    {
+        // GET: ShopItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
