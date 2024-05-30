743f5bd84629c51e2a78a250012beb4a3bc5e1a5
trongkhai <truongtrongkhai77@gmail.com>
2024-05-30 21:11:10 +0700

    pull

---
+++ Fotos/Models/AlbumItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fotos.Models
+{
+    public class AlbumItem
+    {
+        public string Class { get; set; }
+        public string LinkClass { get; set; }
+        public string Title { get; set; }
+    }
+}
